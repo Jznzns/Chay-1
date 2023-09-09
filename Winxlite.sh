41c62cf167ccccf88d15e04207ae58dca5648379
Makabaa <109447791+Makabaa@users.noreply.github.com>
2023-09-09 11:45:38 +0700

    Update Winxlite.sh

--- Winxlite.sh
+++ Winxlite.sh
@@ -1,7 +1,8 @@
 echo "Tai file boot"
 wget -O https://github.com/BlankOn/ovmf-blobs/raw/master/bios64.bin
 echo "Tai xuong iso windows"
-wget -O win.iso https://archive.org/download/windows-x-lite-22621.1928-micro-11-by-fbconan/Windows%20X-Lite%20%2822621.1928%29%20%27Micro%2011%27%20By%20FBConan.iso
+read -p "nhap link tai iso: " CRP
+wget -O win.iso $CRP 
 echo "Tai xuong ngrok"
 wget https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz
 tar -xf ngrok.tgz
