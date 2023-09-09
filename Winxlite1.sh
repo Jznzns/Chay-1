9815ab19545dd21cb4234fc8a1705e1c234e9343
Makabaa <109447791+Makabaa@users.noreply.github.com>
2023-09-09 12:08:14 +0700

    Update Winxlite1.sh

--- Winxlite1.sh
+++ Winxlite1.sh
@@ -1,26 +1,31 @@
-apt-get update
-echo "Tai xuong iso windows"
-wget -O win.iso https://archive.org/download/windows-x-lite-22621.1928-micro-11-by-fbconan/Windows%20X-Lite%20%2822621.1928%29%20%27Micro%2011%27%20By%20FBConan.iso
-echo "Tai xuong ngrok"
-wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
-unzip ngrok-stable-linux-amd64.zip > /dev/null 2>&1
+echo "ban quyen boi https://t.me/chamhoi72"
+echo "boi hoang"
+echo "3"
+echo "2"
+echo "1"
+echo "dang chay"
+echo "chay xong roi"
+sudo su
+wget -O bios64.bin https://github.com/BlankOn/ovmf-blobs/raw/master/bios64.bin
+echo "tai xuong iso"
+read -p "nhap lien ket iso de tai: " CRP
+wget -O windows.iso $CRP
+wget -O ngrok.tgz https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz
 tar -xf ngrok.tgz
 rm -rf ngrok.tgz
-read -p "Nhap Authtoken: " CRP 
-./ngrok authtoken $CRP 
-nohup ./ngrok tcp 5900 &>/dev/null &
-echo Downloading File
-sudo su
-sudo apt update
-sudo apt install qemu-kvm -y
-qemu-img create -f raw win.img 200G
-apt-get install qemu
-echo "Doi"
-echo "Bat Dau Chay Windows"
-echo RDP Address:
-curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
-echo "Ctrl+C De Sao Chep"
-echo "Doi 1-2p De Cho Chay Xong Bot"
-echo "Dung Dong Tab Hay Tat"
-qemu-system-x86_64 -hda win.img -m 8G -smp cores=4 -net user,hostfwd=tcp::5900-:5900 -net nic -object rng-random,id=rng0,filename=/dev/urandom -device virtio-rng-pci,rng=rng0 -vga vmware -nographic
-sleep 43200
+echo "hay di lay token de nhap"
+read -p "nhap token di cu: " CRP
+./ngrok config add-authtoken $CRP
+echo "dang lay ip sau khi co thi qua trang moi,hay tai vnc de nhap ip(sau khi da nhap het lenh)"
+echo "10"
+echo "9"
+echo "8"
+echo "7"
+echo "6"
+echo "5"
+echo "4"
+echo "3"
+echo "2"
+echo "1"
+echo "da co ip"
+./ngrok tcp 5900
