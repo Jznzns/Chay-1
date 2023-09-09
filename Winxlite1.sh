679d9b250547ee1b8e8652122a04d78dfc772446
Makabaa <109447791+Makabaa@users.noreply.github.com>
2023-09-09 17:03:47 +0700

    Update Winxlite1.sh

--- Winxlite1.sh
+++ Winxlite1.sh
@@ -1,16 +1,34 @@
-echo "tai bios"
+echo "Tải bios"
 wget -O bios64.bin https://github.com/BlankOn/ovmf-blobs/raw/master/bios64.bin
-read -p "nhap lien ket iso de tai: " CRP
-wget -O windows.iso $CRP
-echo "tai ngrok"
+read -p "Nhập liên kết iso để sử dụng: " iso
+wget -O windows.iso $iso
+echo "Tải ngrok"
 wget -O ngrok.tgz https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz
 echo "..."
 tar -xf ngrok.tgz
 echo "..."
 rm -rf ngrok.tgz
-echo "hay di lay token de nhap"
-read -p "nhap token di cu: " CRP
-./ngrok config add-authtoken $CRP
-echo "dang lay ip sau khi co thi qua trang moi,hay tai vnc de nhap ip(sau khi da nhap het lenh)"
+echo "Hãy lấy đi lấy token"
+read -p "Nhập token đi kid: " token
+./ngrok config add-authtoken $token
+echo "Khi nhập xong lệnh thì tạo bash mới)"
 echo "..."
-./ngrok tcp 5900
+echo "Nhập vùng muốn chọn (VN chưa hỗ trợ)"
+echo "danh sách mã vùng nên xài"
+echo "- us: Hoa Kỳ (United States)
+- eu: Châu Âu (Europe)
+- ap: Châu Á - Thái Bình Dương (Asia/Pacific)
+- au: Úc (Australia)
+- sa: Nam Mỹ (South America)
+- jp: Nhật Bản (Japan)
+- in: Ấn Độ (India)
+- kh: Cam-pu-chia (Campuchia)
+- sg: Xin-ga-po (Singapore)
+- cn: Trung Quốc (China)
+- ph: Phi-líp-pin (Philippines)
+- la: Lào (Laos)
+- ngrok.io: Vùng mặc định
+Ví dụ, để chọn region là Châu Âu (Europe), bạn có thể sử dụng lệnh sau:
+ngrok region eu"
+read -p "Nhập vùng muốn sử dụng: " region_code
+./ngrok tcp -region "$region_code" 5900
