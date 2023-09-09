ee94de7ed3d859beb7af88bb3bef55d1ad7d9bc3
Makabaa <109447791+Makabaa@users.noreply.github.com>
2023-09-09 16:55:28 +0700

    Update Winxlite2.sh

--- Winxlite2.sh
+++ Winxlite2.sh
@@ -1,9 +1,10 @@
-echo "Dang update"
+echo "Đang tải"
 sudo apt update
-echo "Dang update"
+echo "Đang tải"
 sudo apt install qemu-kvm
-echo "chọn dung luong"
-read -p "Nhap dung luong,VD:128G(nhap xong hay vo nhanh): " CRP
+echo "Nên nhập 128G trở xuống(nhưng có thể tự chọn như 100000G"
+read -p "Nhập dung lượng để xài,VD:128G(nhập xong vô nhanh): " CRP
 qemu-img create -f raw windows.img $CRP
-echo "hay vo nhanh vnc nhanh va an nut bat ky de boot"
+echo "hãy vô vnc và ấn nút bất kỳ để boot"
+echo "bạn đã có thể thưởng thức 1 con vps miễn phí với 1h hoặc hơn để sử dụng như đào coi và vv"
 sudo qemu-system-x86_64 -m 8G -cpu host -boot order=c -drive file=windows.iso,media=cdrom -drive file=windows.img,format=raw -device usb-ehci,id=usb,bus=pci.0,addr=0x4 -device usb-tablet -vnc :0 -smp cores=4 -device rtl8139,netdev=n0 -netdev user,id=n0 -vga qxl -accel kvm -bios bios64.bin
