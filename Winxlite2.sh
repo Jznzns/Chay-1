cb6b104dbe054d09d6cf9c5e7b0e5230536b2729
Makabaa <109447791+Makabaa@users.noreply.github.com>
2023-09-09 12:34:36 +0700

    Update Winxlite2.sh

--- Winxlite2.sh
+++ Winxlite2.sh
@@ -3,7 +3,7 @@ sudo apt update
 echo "Dang update"
 sudo apt install qemu-kvm
 echo "chọn dung luong"
-read -p "Nhap dung luong,VD:128G" CRP
+read -p "Nhap dung luong,VD:128G(nhap xong hay vo nhanh): " CRP
 qemu-img create -f raw win.img $CRP
 echo "hay vo nhanh vnc nhanh va an nut bat ky de boot"
 sudo qemu-system-x86_64 -m 8G -cpu host -boot order=c -drive file=win.iso,media=cdrom -drive file=win.img,format=raw -device usb-ehci,id=usb,bus=pci.0,addr=0x4 -device usb-tablet -vnc :0 -smp cores=4 -device rtl8139,netdev=n0 -netdev user,id=n0 -vga qxl -accel kvm -bios bios64.bin
