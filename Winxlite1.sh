d13a7e16e2cfb7c4327157aa0602b5e76ad8cf1e
Makabaa <109447791+Makabaa@users.noreply.github.com>
2023-09-10 07:54:17 +0700

    Update Winxlite1.sh

--- Winxlite1.sh
+++ Winxlite1.sh
@@ -1,23 +1,30 @@
+clear
 echo "Tải bios"
+sleep 1 &>/dev/null &
 wget -O bios64.bin https://github.com/BlankOn/ovmf-blobs/raw/master/bios64.bin
 read -p "Nhập liên kết iso để sử dụng: " iso
 wget -O windows.iso $iso
 echo "Iso đã tải xong"
-sleep 3
+sleep 1 &>/dev/null &
+clear
 echo "Tải ngrok"
-sleep 1
+sleep 1 &>/dev/null &
+clear
 wget -O ngrok.tgz https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz
 echo "..."
+sleep 1 &>/dev/null &
 tar -xf ngrok.tgz
-echo "..."
 rm -rf ngrok.tgz
-echo "Hãy lấy đi lấy token"
+echo "Hãy đi lấy token"
 read -p "Nhập token đi kid: " token
 ./ngrok config add-authtoken $token
+sleep 1 &>/dev/null &
+clear
 echo "Khi nhập xong lệnh thì tạo bash mới)"
-sleep 1
+sleep 1 &>/dev/null &
 echo "..."
-sleep 1
+sleep 1 &>/dev/null &
+clear
 echo "Nhập vùng muốn chọn (VN chưa hỗ trợ)"
 echo "danh sách mã vùng nên xài"
 echo "- us: Hoa Kỳ (United States)
@@ -35,5 +42,5 @@ echo "- us: Hoa Kỳ (United States)
 - ngrok.io: Vùng mặc định
 Ví dụ, để chọn region là Châu Âu (Europe), bạn có thể sử dụng lệnh sau:
 ngrok region eu"
-read -p "Nhập vùng muốn sử dụng: " region_code
-./ngrok tcp -region "$region_code" 5900
+read -p "Nhập vùng muốn sử dụng: " region
+./ngrok tcp -region $region 5900
