fcc870e99078438ede3fdb96ddfac9d46ed6de15
Makabaa <109447791+Makabaa@users.noreply.github.com>
2023-09-13 16:04:02 +0700

    Update Winxlite3.sh

--- Winxlite3.sh
+++ Winxlite3.sh
@@ -30,8 +30,8 @@ sudo apt install qemu-kvm -y
 sleep 2 &>/dev/null &
 clear
 echo "Chữ G hãy viết Hoa nếu không khi setup sẽ bị lỗi"
-read -p "Nhập dung lượng cho ổ cd-rom: " cd_rom
-qemu-img create -f raw windows.img $cd_rom
+read -p "Nhập dung lượng cho ổ cd-rom: " G
+qemu-img create -f raw win.img $G
 sleep 2 &>/dev/null &
 clear
 echo "Đã tạo xong thành công"
